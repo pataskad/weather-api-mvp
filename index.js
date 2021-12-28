340d0cc60b7306f0e213d01441b493db3f74ff04
Dallas Pataska <pataskad@gmail.com>
2021-12-28 14:18:10 -0600

    error handling to weather api fetch, some styling adjustments

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ let searchString = "La Crosse";
 let weatherUnits = "imperial"; // toggle units for api call; imperial/metric options
 
 async function getWeather() { // syntactic sugar for promises!
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchString}&appid=dab7713d41275b858f22f7809ce1dbb8&units=${weatherUnits}`, {mode: "cors"});
-    const searchData = await response.json();
-    console.log(searchData);
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchString}&appid=dab7713d41275b858f22f7809ce1dbb8&units=${weatherUnits}`, {mode: "cors"}); 
+        const searchData = await response.json();
+        if (searchData.cod === "400" || searchData.cod === "404") {
+            console.log(searchData.cod + " => Please enter a valid or higher populated city");
+        } else {
+            console.log(searchData); // testing 
+            return searchData;
+        }
+    } catch (err) {
+        console.log(err + " **Error caught!**");
+    }
 }
 getWeather();
 
